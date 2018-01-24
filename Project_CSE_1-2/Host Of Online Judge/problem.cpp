94de3213b5540d51d7d0b62b80141b4fe3a13bcf
rathijitpapon <rathijitpapon@users.noreply.github.com>
2018-01-24 22:34:36 +0600

    commit75

--- Project_CSE_1-2/Host Of Online Judge/problem.cpp
+++ Project_CSE_1-2/Host Of Online Judge/problem.cpp
@@ -1,25 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int arr[105];
+
 int main()
 {
     int T;
     cin>>T;
     while(T--)
     {
-        int i, j, k, x, y, z, n;
+        int a=1, x, b=1, c=0, f=0, i, j, k, n;
         cin>>n;
         for(i=0; i<n; i++)
         {
-            cin>>y;
-            if(y>11){ cout<<"YES"<<endl; }
-            else
-            {
-                if(y==3||y==6||y==7||y==9||y==10){ cout<<"YES"<<endl; }
-                else{ cout<<"NO"<<endl; }
-            }
+            cin>>arr[i];
         }
+        for(i=0; i<n; i++)
+        {
+            if(arr[i]==1){a=1; swap(b, c);}
+            else if(arr[i]==2){b=1; swap(a, c);}
+            else if(arr[i]==3){c=1; swap(a, b);}
+            if(a==1&&b==1&&c==1){cout<<"NO"; f=1; break;}
+        }
+        if(f==0){cout<<"YES";}
     }
     return 0;
+
 }
 
